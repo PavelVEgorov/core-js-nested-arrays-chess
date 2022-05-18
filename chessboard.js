044a6adf2b1b1937ccdd391366c4612e302ab517
Pavel Egorov <egorovpvspb@gmail.com>
2022-05-18 13:19:46 +0300

    готов один массив

--- chessboard.js
+++ chessboard.js
@@ -2,8 +2,21 @@ function generateChessboard() {
     const figures = ['Ладья', 'Конь', 'Слон', 'Ферзь', 'Король', 'Слон', 'Конь', 'Ладья'];
     const pawns = new Array(8).fill('Пешка');
     
+    const board = [];
+    const kletka = new Array(8).fill(null)
+
+    for (let i = 0; i < 8; i++) {
+     board.push(kletka);
+    }
+
+    for (let i = 0; i < figures.length; i++){
+      board[0] = figures;
+    }
+    console.log(board)
     // Тут твой код. Верни массив массивов.
 }
+
+console.log(generateChessboard())
 
 function renderChessboard() {
     const emojis = {
@@ -25,3 +38,4 @@ function renderChessboard() {
 }
 
 module.exports = {generateChessboard, renderChessboard};
+
