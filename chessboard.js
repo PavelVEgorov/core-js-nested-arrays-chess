9221d91f46246379a94f9a252eb912e619d9eb4f
Pavel Egorov <egorovpvspb@gmail.com>
2022-05-29 12:52:35 +0300

    Релиз 0. Шахматная доска

--- chessboard.js
+++ chessboard.js
@@ -1,22 +1,29 @@
 function generateChessboard() {
     const figures = ['Ладья', 'Конь', 'Слон', 'Ферзь', 'Король', 'Слон', 'Конь', 'Ладья'];
     const pawns = new Array(8).fill('Пешка');
-    
-    const board = [];
-    const kletka = new Array(8).fill(null)
-
-    for (let i = 0; i < 8; i++) {
-     board.push(kletka);
-    }
-
-    for (let i = 0; i < figures.length; i++){
-      board[0] = figures;
-    }
-    console.log(board)
     // Тут твой код. Верни массив массивов.
-}
 
-console.log(generateChessboard())
+    const whiteFigures = figures.map((el) => 'Б '+ el ); // ряд с белыми фигурами
+    const whitePawns = pawns.map((el) => 'Б '+ el ); // ряд с белыми пешками
+    const blackFigures = figures.map((el) => 'Ч '+ el ); // ряд с черными фигурами
+    const blackPawns = pawns.map((el) => 'Ч '+ el ); // ряд с черными пешками
+
+    const board = []; // новый массив
+    const kletka = new Array(8).fill(null) // мамссив - одна строка доски с 8 клетками с null
+
+    for (let i = 0; i < 8; i++) {  // прогоняем через цикл 8 раз массив строки
+     board.push(kletka);           // пушим в массив board и получаем массив с 8-ю массивами внутри 
+    }                              // где каждое значение - клетка     
+
+    for (let i = 0; i < 8; i++){
+      board[0] = blackFigures;
+      board[1] = blackPawns;
+      board[6] = whitePawns;
+      board[7] = whiteFigures;
+    }
+    return board;
+    // console.log(board)
+}
 
 function renderChessboard() {
     const emojis = {
@@ -35,6 +42,12 @@ function renderChessboard() {
     }
        
     // Тут твой код.
+    const board = generateChessboard(); // доска из функции generateChessboard
+    let newBord = board.map((el) => el.map((item) => emojis[item] || ' ').join('')).join('\n');
+    // console.log(newBord)
+    // console.log(newBord[0])
+    // console.log(newBord[6])
+    return newBord;
 }
 
 module.exports = {generateChessboard, renderChessboard};
